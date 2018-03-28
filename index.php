06351342ce4fc43cee0e70f502d6d5260717bb1d
jhudsonsg <jhudsonsg@gmail.com>
2018-03-28 09:13:59 -0300

    Instance of class for index file

--- index.php
+++ index.php
@@ -5,4 +5,8 @@ ini_set('display_startup_erros',1);
 error_reporting(E_ALL);
 
 require_once "vendor/autoload.php";
-require "routes.php";
+$route = new Router\Router;
+
+$route->get("/", "pedidosController@sampleIndex");
+$route->get("/list", "pedidosController@samplePedidoDescription");
+$route->get("/{foo}/{bar}", "pedidosController@sampleRequest");
--- routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$route = new Router\Router;
-
-$route->get("/", "pedidosController@sampleIndex");
-$route->get("/list", "pedidosController@samplePedidoDescription");
-$route->get("/{foo}/{bar}", "pedidosController@sampleRequest");
