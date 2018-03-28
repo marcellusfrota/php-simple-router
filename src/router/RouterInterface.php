ed1278bafca31ae75980c39c221f4b3f67d3d8e2
jhudsonsg <jhudsonsg@gmail.com>
2018-03-28 09:17:43 -0300

    Commenting interfaceCommenting interface

--- src/router/RouterInterface.php
+++ src/router/RouterInterface.php
@@ -6,16 +6,30 @@ interface RouterInterface
 
 	/**
 	 * Métodos suportados pela aplicação
-	 * 
 	 */
 	const METHOD_GET    = 'GET';
 	const METHOD_POST   = 'POST';
 	const METHOD_PUT    = 'PUT';
 	const METHOD_DELETE = 'DELETE';
 
+	/**
+	 * Method post
+	 */
 	public function post($url, $args);
+
+	/**
+	 * Method get
+	 */
 	public function get($url, $args);
+
+	/**
+	 * Method delete
+	 */
 	public function delete($url, $args);
+
+	/**
+	 * Method put
+	 */
 	public function put($url, $args);
 
 }
