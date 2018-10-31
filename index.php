2c78ff1bfd6953717623e944854fa64eefbf04ef
jhudsonsg <jhudsonsg@gmail.com>
2018-10-31 09:51:08 -0300

    Restructuring of the base project

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-require_once "vendor/autoload.php";
-$route = new Router\Router;
-
-$route->get("/", "pedidosController@sampleIndex");
-$route->get("/list", "pedidosController@samplePedidoDescription");
-$route->get("/{foo}/{bar}", "pedidosController@sampleRequest");
---
+++ src/Router.php
@@ -0,0 +1,10 @@
+<?php 
+namespace App;
+
+use App\Route;
+use App\RouterInterface;
+
+class Router implements RouterInterface
+{
+	
+}
