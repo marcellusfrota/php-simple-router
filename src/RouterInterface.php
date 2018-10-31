bfa95fac7818622a08e9118b921199d8ca4d6be0
jhudsonsg <jhudsonsg@gmail.com>
2018-10-31 10:02:42 -0300

    Defining route interface methods

--- src/RouterInterface.php
+++ src/RouterInterface.php
@@ -3,5 +3,35 @@ namespace App;
 
 interface RouterInterface
 {
+    /**
+     * Method GET
+     * 
+     * @param String $uri
+     * @return
+     */
+    public function get();
 
+    /**
+     * Method POST
+     * 
+     * @param String $uri
+     * @return
+     */
+    public function post();
+
+    /**
+     * Method PUT
+     * 
+     * @param String $uri
+     * @return
+     */
+    public function put();
+
+    /**
+     * Method DELETE
+     * 
+     * @param String $uri
+     * @return
+     */
+    public function delete();
 }
