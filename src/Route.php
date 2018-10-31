48796fd16efd185421cc2d9a95c8940049b84768
jhudsonsg <jhudsonsg@gmail.com>
2018-10-31 10:09:13 -0300

    Construction of the router and model of a route

--- src/Route.php
+++ src/Route.php
@@ -3,5 +3,44 @@ namespace App;
 
 class Route
 {
-    
+    private $uri;
+    private $controller;
+    private $view;
+
+    public function __construct($uri, $controller, $view)
+    {
+        $this->uri        = $uri;
+        $this->controller = $controller;
+        $this->view       = $view;
+    }
+
+    public function getUri()
+    {
+        return $this->uri;
+    }
+
+    public function getController()
+    {
+        return $this->controller;
+    }
+
+    public function getView()
+    {
+        return $this->view;
+    }
+
+    public function setUri($uri)
+    {
+        $this->uri = $uri;
+    }
+
+    public function setController($controller)
+    {
+        $this->controller = $controller;
+    }
+
+    public function setView($view)
+    {
+        $this->view = $view;
+    }
 }
--- src/Router.php
+++ src/Router.php
@@ -6,5 +6,56 @@ use App\RouterInterface;
 
 class Router implements RouterInterface
 {
-	
+    private $routes = Array();
+    
+    /**
+     * @param String $uri
+     * @return
+     */
+    public function get()
+    {
+
+    }
+
+    /**
+     * @param String $uri
+     * @return
+     */
+    public function post()
+    {
+
+    }
+
+    /**
+     * @param String $uri
+     * @return
+     */
+    public function put()
+    {
+
+    }
+
+    /**
+     * @param String $uri
+     * @return
+     */
+    public function delete()
+    {
+
+    }
+
+    /**
+     * Method executes all routes
+     * 
+     * @return
+     */
+    public function exec()
+    {
+
+    }
+
+    public function __destruct()
+    {
+        $this->exec();
+    }
 }
